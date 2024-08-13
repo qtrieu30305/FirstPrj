ec930e770ccddae297979379a9bebb4185432ab1
Nguyễn Quốc Triệu <23697501.trieu@student.iuh.edu.vn>
2024-08-13 09:52:09 +0700

    Nguyen Quoc Trieu
    23697501

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Quoc Trieu");
+        System.out.println("Welcome to the DHHTTT19A!");
+    }
+}
